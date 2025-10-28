f127b05e13ea837728e0898beb9478fbfb3a6e39
Alex <adrewno14@gmail.com>
2025-10-27 20:11:21 -0600

    more nvim config updates

---
+++ lua/alex/set.lua
@@ -0,0 +1,16 @@
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.scrolloff = 8
+
+vim.opt.wrap = true
+
+vim.opt.mouse = c
